9cbfa3db0b8490b61a5b470088d3d2733d4a2b38
felix <git@egofelix.de>
2020-12-19 19:30:10 +0100

    Updated Unlocker

--- unlocker.sh
+++ unlocker.sh
@@ -1,60 +1,58 @@
 #!/bin/bash
-FULLKEY=""
-for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50
-do
-  VARNAME="PRIVKEY${i}"
 
-  if [ -z "${!VARNAME}" ]
-  then
-    break
-  fi
+# Check if HOST_NAME was provided
+if [[ -z ${HOST_NAME:-} ]]; then
+  echo "Please provide the target hostname in HOST_NAME"
+  exit
+fi;
 
-  FULLKEY="${FULLKEY}\n${!VARNAME}"
-done
+# Default Port
+if [[ -z ${HOST_PORT:-} ]]; then
+  HOST_PORT="22"
+fi;
 
-if [ -z "${FULLKEY}" ]
-then
-    echo "Did you forget to fill PRIVKEY environments?"
-    sleep 10
-    exit 1
+# Default User
+if [[ -z ${HOST_USER:-} ]]; then
+  HOST_USER="root"
+fi;
+
+# Check if SSH Public Key is provided
+if [[ -z ${HOST_PUBKEY:-} ]]; then
+  if [[ -f "/data/host.key" ]]; then
+    HOST_PUBKEY=$(cat /data/host.key)
+  fi;
+fi
+if [[ -z ${HOST_PUBKEY:-} ]]; then
+  echo "Please provide the ssh public key of the host in HOST_SSH_PUB_KEY or /data/host.key"
+  exit
+fi;
+
+# Check if private key is provided
+if [[ ! -f "/data/private.key" ]]; then
+  echo "Please provide private-key in /data/private.key"
 fi
 
-if [ -z "${HOST_KEY}" ]
-then
-    echo "Did you forget to fill HOST_KEY environments?"
-    sleep 10
-    exit 2
+# Check if unlock-keyfile is provided
+if [[ -z ${HOST_UNLOCK_KEY:-} ]]; then
+  if [[ -f "/data/unlock.key" ]]; then
+    HOST_UNLOCK_KEY=$(cat /data/unlock.key)
+  fi;
 fi
+if [[ -z ${HOST_UNLOCK_KEY:-} ]]; then
+  echo "Please provide the ssh unlock key in HOST_UNLOCK_KEY or /data/unlock.key"
+  exit
+fi;
 
-if [ -z "${HOST_PORT}" ]
-then
-    echo "Did you forget to fill HOST_PORT environments?"
-    sleep 10
-    exit 3
-fi
+# All checks done
 
-if [ -z "${HOST_USER}" ]
-then
-    echo "Did you forget to fill HOST_USER environments?"
-    sleep 10
-    exit 4
-fi
-
-if [ -z "${HOST_PASS}" ]
-then
-    echo "Did you forget to fill HOST_PASS environments?"
-    sleep 10
-    exit 5
-fi
-
-
-echo -e ${FULLKEY} | base64 -d > /tmp/id_rsa && chmod 0600 /tmp/id_rsa
-echo "[${HOST_NAME}]:${HOST_PORT} $HOST_KEY" > /tmp/known_hosts
-PUBKEY=`ssh-keygen -y -f /tmp/id_rsa`
+# Generate known_hosts file
+mkdir -p ~/.ssh/
+echo "[${HOST_NAME}]:${HOST_PORT} ${HOST_PUBKEY}" > /tmp/known_hosts
+PUBKEY=`ssh-keygen -y -f /data/private.key`
 
 while true; do
   echo "Trying to unlock using key: ${PUBKEY}"
-  echo -n "${HOST_PASS}" | ssh -m hmac-sha2-256 -o "UserKnownHostsFile /tmp/known_hosts" -o "IdentityFile /tmp/id_rsa" -p ${HOST_PORT} ${HOST_USER}@${HOST_NAME} > /dev/null 2>&1
+  echo -n "${HOST_UNLOCK_KEY}" | ssh -o "UserKnownHostsFile /tmp/known_hosts" -o "IdentityFile /tmp/id_rsa" -p ${HOST_PORT} ${HOST_USER}@${HOST_NAME} > /dev/null 2>&1
   sleep 30
 done
 
